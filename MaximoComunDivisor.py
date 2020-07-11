ab859d5fed370cedf7fc76bd049a32e81e1ea10b
dsanchezme <48555473+dsanchezme@users.noreply.github.com>
2020-07-11 09:10:14 -0500

    Update and rename GreatestCommonDivisor.py to MaximoComunDivisor.py

--- MaximoComunDivisor.py
+++ MaximoComunDivisor.py
@@ -1,5 +1,5 @@
 """
-Title: Greater Common Divisor (GCD)
+Title: Máximo Común Divisor (MCD)
 Author: Diego Felipe Sánchez Medina
 Mail: dsanchezme@unal.edu.co
 """
@@ -7,9 +7,9 @@ Mail: dsanchezme@unal.edu.co
 a=64
 b=24
 
-def GCD(a,b):
+def MCD(a,b):
   if(a%b==0):
     return b
-  return GCD(b,a%b)
+  return MCD(b,a%b)
 
-print(GCD(a,b))
+print(MCD(a,b))
